54f9d680760bad64eb11ca434dc8aabf8e123e2d
Rohit Jiwade <rohitjiwade2019@gmail.com>
2023-12-18 14:40:23 +0000

    Merge branch 'main' of https://github.com/RohitSanjayjiwade/Python-Project into main

--- my_app/urls.py
+++ my_app/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from census.views import pincode
+from census.views import pincode #index
 
 urlpatterns = [
+    # path("", index, name="index"),
     path("<pincode>", pincode, name="pincode"),
     path("census-2011/", include("census.urls")),
     path("admin/", admin.site.urls),
